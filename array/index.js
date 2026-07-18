56c159ebb8147b2148ca6c8221def63d2662c2f3
Waghmodess <sachinwaghmode1000@gmail.com>
2026-07-18 12:17:06 +0530

    feat: added function to find the first, second and third largest numbers in an array

--- array/index.js
+++ array/index.js
@@ -1,28 +1,69 @@
-// 1. Find the Largest and Smallest Number in an Array. 
-// A. Without Using Built-in Methods 
+// 1. Find the Largest and Smallest Number in an Array.
+// A. Without Using Built-in Methods
 const arr = [1, 2, 3, 4, 5, 5, 1];
 
 function findMinAndMax(arr) {
-    let max = arr[0];
-    let min = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
-        if (arr[i] < min) {
-            min = arr[i];
-        }
+  let max = arr[0];
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    return { max, min };
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return { max, min };
 }
 console.log(findMinAndMax(arr));
 
 // B. Using Built-in Methods (Math.max() & Math.min())
 function findMinAndMaxUsingMath(arr) {
-    const max = Math.max(...arr);
-    const min = Math.min(...arr);
-    return { max, min };
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  return { max, min };
 }
 console.log(findMinAndMaxUsingMath(arr));
 
 // ------------------------------------------------------------------------------
+
+// 2. Find the first, second and third largest numbers in an array
+// A. Without Using Built-in Methods
+let array = [1, 2, 3, 4, 5, 5, 4, 3, 2, 1];
+function findThreeLargestDistinct(array) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+  let thirdLargest = -Infinity;
+
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    if (num > largest) {
+      thirdLargest = secondLargest;
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest && num !== largest) {
+      thirdLargest = secondLargest;
+      secondLargest = num;
+    } else if (num > thirdLargest && num !== largest && num !== secondLargest) {
+      thirdLargest = num;
+    }
+  }
+  return { largest, secondLargest, thirdLargest };
+}
+console.log(findThreeLargestDistinct(array));
+
+// B. Using Built-in Methods (Math.max() & Math.min())
+function findSecondAndThirdLargest(arr) {
+    const sorted = [...new Set(arr)].sort((a, b) => b - a);
+    console.log(sorted);
+
+    return {
+        secondLargest: sorted[1],
+        thirdLargest: sorted[2],
+    };
+}
+
+console.log(findSecondAndThirdLargest(arr));
+
+// ------------------------------------------------------------------------------
+
